bb6328da0a2084a7decf5d3fc1f593d9455e41ac
jubilee2 <jubilee2@gmail.com>
2026-08-27 22:25:28 -0500

    Implement VPN permission flow

--- app/src/main/java/com/localfirewall/app/MainActivity.kt
+++ app/src/main/java/com/localfirewall/app/MainActivity.kt
@@ -1,15 +1,54 @@
 package com.localfirewall.app
 
+import android.app.Activity
+import android.content.Intent
+import android.net.VpnService
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.compose.setContent
+import androidx.core.content.ContextCompat
 import com.localfirewall.app.ui.AndroidLocalFirewallApp
+import com.localfirewall.app.vpn.FirewallVpnService
+import com.localfirewall.app.vpn.VpnPermissionAction
+import com.localfirewall.app.vpn.VpnPermissionDecision
 
 class MainActivity : ComponentActivity() {
+    private val vpnPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+    ) { result ->
+        handlePermissionAction(
+            VpnPermissionDecision.afterPermissionResult(result.resultCode == Activity.RESULT_OK),
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidLocalFirewallApp()
+            AndroidLocalFirewallApp(onStartFirewall = ::requestVpnPermission)
         }
+    }
+
+    private fun requestVpnPermission() {
+        val permissionIntent = VpnService.prepare(this)
+        when (VpnPermissionDecision.afterPrepare(permissionIntent != null)) {
+            VpnPermissionAction.RequestPermission ->
+                vpnPermissionLauncher.launch(requireNotNull(permissionIntent))
+            VpnPermissionAction.StartService -> startFirewallService()
+            VpnPermissionAction.KeepFirewallOff -> Unit
+        }
+    }
+
+    private fun handlePermissionAction(action: VpnPermissionAction) {
+        when (action) {
+            VpnPermissionAction.StartService -> startFirewallService()
+            VpnPermissionAction.RequestPermission,
+            VpnPermissionAction.KeepFirewallOff,
+            -> Unit
+        }
+    }
+
+    private fun startFirewallService() {
+        ContextCompat.startForegroundService(this, Intent(this, FirewallVpnService::class.java))
     }
 }
